70317c98a482aa9a2a2530a0275679b599ea4c89
James Cleworth <jamescleworth@hotmail.co.uk>
2015-03-13 21:01:38 +0000

    added fade effect

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,14 @@
-document.querySelector( "#hamburger-menu" ).addEventListener( "click", function() {
-	this.classList.toggle( "active" );
-}); 
+document.querySelector( "#hamburger-menu" )
+  .addEventListener( "click", function() {
+    this.classList.toggle( "active" );
+  });
+
+
+  function effect (){
+
+
+if ($('.topBanner > h3').css('opacity') == 1) {
+    alert('do something');
+}
+
+}
