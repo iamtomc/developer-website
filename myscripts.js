5bf1810403e34da3a1b9b0b499c9720c3e9cc29c
James Cleworth <jamescleworth@hotmail.co.uk>
2015-03-16 11:14:47 +0000

    menu updated with word changing and page moves

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,4 @@
-document.querySelector( "#hamburger-menu" )
+document.querySelector( "#hamburger-menu")
   .addEventListener( "click", function() {
     this.classList.toggle( "active" );
     $('.sidebar').css({'width': '200px'});
